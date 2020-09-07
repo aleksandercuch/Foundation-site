9d60109634a439289259b619bb5aa766d1f3ccec
Aleksander <cuch.aleksander@gmail.com>
2020-09-07 12:51:15 +0200

    initial commit with navbar

---
+++ foundation/src/components/navbar/navbar.js
@@ -0,0 +1,144 @@
+import React, { Component } from 'react';
+import { NavLink } from "react-router-dom"
+
+import Hidden from '@material-ui/core/Hidden';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+import Link from '@material-ui/core/Link';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import YouTubeIcon from '@material-ui/icons/YouTube';
+
+import SmallNavbar from './smallNavbar';
+import "./navbar.css";
+
+class Navbar extends Component {
+
+	render() {
+		return (
+			<Grid
+				container
+				direction="row-reverse"
+				justify="center"
+				alignItems="flex-start"
+			>
+				<Grid item xs={9}>
+					<Grid
+						container
+						direction="row"
+						justify="center"
+						alignItems="center"
+						spacing={5}
+					>
+						<Grid item xs={12}>
+							<Grid
+								container
+								direction="row"
+								justify="center"
+								alignItems="center"
+							>
+								<Grid item md={10} xs={11}>
+									<Grid
+										container
+										direction="column"
+										justify="flex-end"
+										alignItems="center"
+									>
+										<Grid item xs={12}>
+											<NavLink to="/" style={{ 'color': 'inherit', 'textDecoration': 'none' }}>
+												<Hidden smDown>
+													<Box fontSize="h2.fontSize" fontWeight="fontWeightLight" fontFamily="Monospace">
+														FUNDACJA POMOST
+													</Box>
+												</Hidden>
+												<Hidden mdUp>
+													<Box fontSize="h4.fontSize" fontWeight="fontWeightLight" fontFamily="Monospace">
+														FUNDACJA POMOST
+													</Box>
+												</Hidden>
+											</NavLink>
+										</Grid>
+									</Grid>
+								</Grid>
+								<Grid item md={2} xs={1}>
+									<Hidden mdDown>
+										<Grid
+											container
+											direction="row"
+											justify="flex-end"
+											alignItems="flex-start"
+										>
+											<Grid item xs={4}>
+												<Link href="https://www.facebook.com/fundacjapomost/" color="textSecondary" target="_blank">
+													<FacebookIcon fontSize="large" />
+												</Link>
+											</Grid>
+											<Grid item xs={4}>
+												<Link href="https://twitter.com/fundacja_pomost?lang=pl" color="textSecondary" target="_blank">
+													<TwitterIcon fontSize="large" />
+												</Link>
+											</Grid>
+											<Grid item xs={4}>
+												<Link href="https://www.youtube.com/c/FundacjaPomost" color="textSecondary" target="_blank">
+													<YouTubeIcon fontSize="large" />
+												</Link>
+											</Grid>
+										</Grid>
+									</Hidden>
+									<Hidden lgUp>
+										<SmallNavbar />
+									</Hidden>
+								</Grid>
+							</Grid>
+						</Grid>
+						<Grid item xs={12}>
+							<Hidden mdDown>
+								<nav>
+									<ul>
+										<li class="menu">
+											<NavLink to="/o-fundacji">O FUNDACJI</NavLink>
+											<ul class="subMenu aboutMenu">
+												<li><NavLink to="/misja">misja</NavLink></li>
+												<li><NavLink to="/co-robimy">co robimy?</NavLink></li>
+												<li><NavLink to="/zespol">zespół</NavLink></li>
+												<li><NavLink to="/projekty">projekty</NavLink></li>
+												<li><NavLink to="/sprawozdania">sprawozdania</NavLink></li>
+											</ul>
+										</li>
+										<li class="menu">
+											<NavLink to="/osrodek-readaptacji">OŚRODEK READAPTACJI</NavLink>
+											<ul class="subMenu readaptationMenu">
+												<li><NavLink to="/dla-kogo">Dla kogo?</NavLink></li>
+												<li><NavLink to="/zalozenia">Założenia</NavLink></li>
+												<li><a href="#">Program</a>
+													<ul class="superSubMenu">
+														<li><NavLink to="/punkt-konsultacyjny">punkt konsultacyjny</NavLink></li>
+														<li><NavLink to="/etap-readaptacji">etap readaptacji</NavLink></li>
+														<li><NavLink to="/etap-postrehabilitacji">etap postrehabilitacji</NavLink></li>
+														<li><NavLink to="/grupy-wsparcia">grupy wsparcia</NavLink></li>
+													</ul>
+												</li>
+												<li><NavLink to="/o-metodzie">o metodzie</NavLink></li>
+											</ul>
+										</li>
+										<li class="menu"><NavLink to="/oferta-szkolen">OFERTA SZKOLEŃ</NavLink></li>
+										<li class="menu"><NavLink to="/potrzebujesz-wsparcia">POTRZEBUJESZ WSPARCIA?</NavLink></li>
+
+										<li class="menu"><NavLink to="/chcesz-pomoc">CHCESZ POMÓC?</NavLink></li>
+										<li class="menu"><NavLink to="/blog">BLOG</NavLink></li>
+										<li class="menu"><NavLink to="/kontakt">KONTAKT</NavLink></li>
+									</ul>
+								</nav>
+							</Hidden>
+						</Grid>
+					</Grid>
+				</Grid>
+				<Grid item md={1} sm={2} xs={4}>
+					<img className="saturate" src="https://static.wixstatic.com/media/ed491c_ce6e670531c04532b5ea9a3e11f84bd0~mv2_d_1250_1250_s_2.png/v1/fill/w_138,h_137,al_c,q_85,usm_0.33_1.00_0.00/logo_opp_czarne2.webp" alt="logo error"></img>
+				</Grid>
+			</Grid>
+		);
+	}
+}
+
+export default Navbar;
---
+++ foundation/src/components/navbar/smallNavbar.js
@@ -0,0 +1,245 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom'
+
+import { makeStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import List from '@material-ui/core/List';
+import Box from '@material-ui/core/Box';
+import ListItem from '@material-ui/core/ListItem';
+import MenuIcon from '@material-ui/icons/Menu';
+import Divider from '@material-ui/core/Divider';
+import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
+import ArrowDropUpIcon from '@material-ui/icons/ArrowDropUp';
+
+const useStyles = makeStyles({
+  list: {
+    width: 250,
+  },
+  fullList: {
+    width: 'auto',
+  },
+});
+
+function subMenuItem(name) {
+  return (
+    <Grid
+      container
+      direction="row"
+      justify="flex-start"
+      alignItems="center"
+    >
+      <Grid item >
+        <ArrowDropUpIcon fontSize="inherit" />
+      </Grid>
+      <Grid item xs={8}>
+        {name}
+      </Grid>
+    </Grid>
+  )
+}
+
+export default function TemporaryDrawer() {
+  const classes = useStyles();
+  const [state, setState] = React.useState({
+    top: false,
+    left: false,
+    bottom: false,
+    right: false,
+  });
+
+  const toggleDrawer = (side, open) => event => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+
+    setState({ ...state, [side]: open });
+  };
+
+  const fullList = side => (
+    <div
+      className={classes.fullList}
+      role="presentation"
+      onClick={toggleDrawer(side, false)}
+      onKeyDown={toggleDrawer(side, false)}
+    >
+      <List>
+        <ListItem >
+          <Box fontWeight="fontWeightBold">
+            <NavLink to="/o-fundacji" style={{ 'color': 'inherit', 'textDecoration': 'none' }}>
+              O fundacji
+            </NavLink>
+            <List>
+              <ListItem button="true">
+                <Box fontWeight="fontWeightBold">
+                  <NavLink to="/misja" style={{ 'color': 'inherit', 'textDecoration': 'none' }}>
+                    {subMenuItem("Misja")}
+                  </NavLink>
+                </Box>
+              </ListItem>
+              <ListItem button="true">
+                <Box fontWeight="fontWeightBold">
+                  <NavLink to="/co-robimy" style={{ 'color': 'inherit', 'textDecoration': 'none' }}>
+                    {subMenuItem("Co robimy?")}
+                  </NavLink>
+                </Box>
+              </ListItem>
+              <ListItem button="true">
+                <Box fontWeight="fontWeightBold">
+                  <NavLink to="/zespol" style={{ 'color': 'inherit', 'textDecoration': 'none' }}>
+                    {subMenuItem("Zespół")}
+                  </NavLink>
+                </Box>
+              </ListItem>
+              <ListItem button="true">
+                <Box fontWeight="fontWeightBold">
+                  <NavLink to="projekty" style={{ 'color': 'inherit', 'textDecoration': 'none' }}>
+                    {subMenuItem("Projekty")}
+                  </NavLink>
+                </Box>
+              </ListItem>
+              <ListItem button="true">
+                <Box fontWeight="fontWeightBold">
+                  <NavLink to="sprawozdania" style={{ 'color': 'inherit', 'textDecoration': 'none' }}>
+                    {subMenuItem("Sprawozdania")}
+                  </NavLink>
+                </Box>
+              </ListItem>
+            </List>
+          </Box>
+        </ListItem>
+        <Divider />
+        <ListItem>
+          <Box fontWeight="fontWeightBold">
+            <NavLink to="/osrodek-readaptacji" style={{ 'color': 'inherit', 'textDecoration': 'none' }}>
+              Ośrodek readaptacji
+            </NavLink>
+            <List>
+              <ListItem button="true">
+                <Box fontWeight="fontWeightBold">
+                  <NavLink to="/dla-kogo" style={{ 'color': 'inherit', 'textDecoration': 'none' }}>
+                    {subMenuItem("Dla kogo?")}
+                  </NavLink>
+                </Box>
+              </ListItem>
+              <ListItem button="true">
+                <Box fontWeight="fontWeightBold">
+                  <NavLink to="/zalozenia" style={{ 'color': 'inherit', 'textDecoration': 'none' }}>
+                    {subMenuItem("Założenia")}
+                  </NavLink>
+                </Box>
+              </ListItem>
+              <ListItem>
+                <Box fontWeight="fontWeightBold">
+                  <NavLink to="/zespol" style={{ 'color': 'inherit', 'textDecoration': 'none' }}>
+                    {subMenuItem("Program")}
+                  </NavLink>
+                  <List>
+                    <ListItem button="true">
+                      <Box fontWeight="fontWeightBold">
+                        <NavLink to="/co-robimy" style={{ 'color': 'inherit', 'textDecoration': 'none' }}>
+                          {subMenuItem("Punkt konsultacyjny")}
+                        </NavLink>
+                      </Box>
+                    </ListItem>
+                    <ListItem button="true">
+                      <Box fontWeight="fontWeightBold">
+                        <NavLink to="/co-robimy" style={{ 'color': 'inherit', 'textDecoration': 'none' }}>
+                          {subMenuItem("Etap readaptacji")}
+                        </NavLink>
+                      </Box>
+                    </ListItem>
+                    <ListItem button="true">
+                      <Box fontWeight="fontWeightBold">
+                        <NavLink to="/co-robimy" style={{ 'color': 'inherit', 'textDecoration': 'none' }}>
+                          {subMenuItem("Etap postrehabilitacji")}
+                        </NavLink>
+                      </Box>
+                    </ListItem>
+                    <ListItem button="true">
+                      <Box fontWeight="fontWeightBold">
+                        <NavLink to="/co-robimy" style={{ 'color': 'inherit', 'textDecoration': 'none' }}>
+                          {subMenuItem("Grupy wsparcia")}
+                        </NavLink>
+                      </Box>
+                    </ListItem>
+                  </List>
+                </Box>
+              </ListItem>
+              <ListItem button="true">
+                <Box fontWeight="fontWeightBold">
+                  <NavLink to="/o-metodzie" style={{ 'color': 'inherit', 'textDecoration': 'none' }}>
+                    <Grid
+                      container
+                      direction="row"
+                      justify="flex-start"
+                      alignItems="center"
+                    >
+                      <Grid item >
+                        <ArrowDropUpIcon fontSize="inherit" />
+                      </Grid>
+                      <Grid item >
+                        O metodzie
+                      </Grid>
+                    </Grid>
+                  </NavLink>
+                </Box>
+              </ListItem>
+            </List>
+          </Box>
+        </ListItem>
+        <Divider />
+        <ListItem button="true">
+          <Box fontWeight="fontWeightBold">
+            <NavLink to="/oferta-szkolen" style={{ 'color': 'inherit', 'textDecoration': 'none' }}>
+              Oferta szkoleń
+            </NavLink>
+          </Box>
+        </ListItem>
+        <Divider />
+        <ListItem button="true">
+          <Box fontWeight="fontWeightBold">
+            <NavLink to="/potrzebujesz-wsparcia" style={{ 'color': 'inherit', 'textDecoration': 'none' }}>
+              Potrzebujesz wsparcia?
+            </NavLink>
+          </Box>
+        </ListItem>
+        <Divider />
+        <ListItem button="true">
+          <Box fontWeight="fontWeightBold">
+            <NavLink to="/chcesz-pomoc" style={{ 'color': 'inherit', 'textDecoration': 'none' }}>
+              Chcesz pomóc?
+            </NavLink>
+          </Box>
+        </ListItem>
+        <Divider />
+        <ListItem button="true">
+          <Box fontWeight="fontWeightBold">
+            <NavLink to="/blog" style={{ 'color': 'inherit', 'textDecoration': 'none' }}>
+              Blog
+            </NavLink>
+          </Box>
+        </ListItem>
+        <Divider />
+        <ListItem button="true">
+          <Box fontWeight="fontWeightBold">
+            <NavLink to="/kontakt" style={{ 'color': 'inherit', 'textDecoration': 'none' }}>
+              Kontakt
+            </NavLink>
+          </Box>
+        </ListItem>
+        <Divider />
+      </List>
+    </div>
+  );
+
+  return (
+    <div>
+      <Button onClick={toggleDrawer('left', true)}><MenuIcon /></Button>
+      <Drawer anchor="left" open={state.left} onClose={toggleDrawer('left', false)}>
+        {fullList('left')}
+      </Drawer>
+    </div>
+  );
+}
